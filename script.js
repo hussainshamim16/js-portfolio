20257bb0a192c191f61ae5acf7c845040734ceab
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-06-05 09:13:02 +0500

    takreeban

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ function unfilter(atribute){
     atribute.src = "./Snapchat-475541476.jpg"
     console.log("nhi Hoa")
 }
+function iner(sub){
+// console.log(sub)
+sub.style.marginLeft = "30px"
+sub.style.transitionDuration = "2s"
+sub.style.boxShadow= "2px 3px 30px 0px #a8a8a8"
+}
+function outer(fol){
+    fol.style.marginLeft = "0px"
+    fol.style.transitionDuration = "2s"
+    fol.style.boxShadow= "none"
+}
+
+var peragraph = document.getElementById("pera")
+
+peragraph.addEventListener("click",function abc(){
+    console.log("red")
+    for(var i = 0; i<=peragraph.lenght; i++){
+        console.log(peragraph[i])
+    }
+})
